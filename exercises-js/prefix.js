a0cf24cb3bacaad5257028cc9e3a225eba8f30eb
edusamico <edusamico@gmail.com>
2022-03-04 16:02:55 -0300

    review

--- exercises-js/prefix.js
+++ exercises-js/prefix.js
@@ -4,32 +4,24 @@
  */
  var longestCommonPrefix = function(strs) {
     var strPref = "";
-    
-    if (strs.length === 0 || strs[0] === "") return strPref;
-    if ( strs.length === 1) {
-        strPref = strs[0];
-        return strPref;
-    }
+
     for (let i = 0; i <= strs[0].length - 1; i++){
         let letter = strs[0][i];
         for (let j in strs){
             if (strs[j][i] !== letter) {
                 return strPref;
-                } 
+            } 
         }
-
         strPref+= letter;      
-        if (strPref === strs[0]) {
-            return strPref;
-        }
     }
-    
-  ;
+    return strPref;
 };
 
-var strs = ["flower","flower","flower","flower"]
+var strs = ["", "a","a"]
+// var strs = ["flower","flower","flower","flower"]
+// var strs = ["a"];
 // var strs = [""]
-// var strs = ["a"]
+
 // var strs = ["flower","flow","flight"];
 var play = longestCommonPrefix(strs);
 console.log(play);
